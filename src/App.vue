82c267d5fcb1f25cb3f29f876f68ade661f407c9
liuxu <1173430037@qq.com>
2023-09-20 15:34:29 +0800

    add feature-liuxu

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>姓名：刘旭</h1>
+  <h1>学号：2310275003</h1>
+  <img alt="pika" src="./assets/pika.jpg">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
